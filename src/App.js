c0637ccf638fd08633e1824bdd91e0a4cf3e643a
rhobbikhaidir <rhobbie09@gmail.com>
2021-11-29 11:02:04 +0700

    add dark mode

--- src/App.js
+++ src/App.js
@@ -1,122 +1,165 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useEffect } from "react/cjs/react.development";
 import CoinCard from "./components/CoinCard";
 import "./styles/tailwind.css";
 function App() {
-  const [date, setDate] = useState(new Date());
+  const handleChangeTheme = (value) => {
+    localStorage.setItem("theme", value);
+    const html = document.querySelector("html");
+
+    if (value === "dark") {
+      html.classList.remove("light");
+    } else {
+      html.classList.remove("dark");
+    }
+    html.classList.add(localStorage.getItem("theme"));
+  };
+
   useEffect(() => {
-    const timer = setInterval(() => {
-      setDate(new Date());
-      console.log(date.getSeconds());
-    }, 60 * 1000);
-    console.log(date.getMinutes());
-    return () => clearInterval(timer);
-  }, [date]);
+    document.querySelector("html").classList.add(localStorage.getItem("theme"));
+  });
   return (
-    <div className="max-w-sm mr-0 block sm:flex sm:justify-between sm:max-w-full sm:py-16 sm:px-8 py-4 px-4 sm:space-x-8">
-      <div className="mb-8  flex-1 flex-col sm:ml-3 sm:flex-row  ">
-        {/* content realtime */}
-        <div className="flex justify-between space-x-0.5 sm:space-x-6 max-w-xs sm:w-full ">
-          <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg ">
-            <h1 className="text-2xl mt-2 sm:text-5xl flex items-end ml-4 sm:mt-6 text-center font-normal text-black">
-              01{" "}
-              <span className="text-xs text-center mb-2 sm:text-sm flex-row flex-1 ml-1 mt-6 sm:mr-12 text-gray-700">
-                Day
-              </span>
-            </h1>
-          </div>
-          <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg ">
-            <h1 className="text-2xl mt-2 sm:text-5xl flex items-end ml-2 sm:mt-6 text-center font-normal text-black">
-              01{" "}
-              <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1 ml-1 mt-6 sm:mr-12 text-gray-700">
-                Hours
-              </span>
-            </h1>
-          </div>
-          <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg ">
-            <h1 className="text-2xl mt-2 sm:text-5xl flex items-end ml-2 sm:mt-6 text-center font-normal text-black">
-              00{" "}
-              <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1  mt-6 sm:mr-10 text-gray-700">
-                Minute
-              </span>
-            </h1>
-          </div>
-          <div className="rounded-3xl flex-1 sm:h-20 shadow-lg ">
-            <h1 className="text-2xl mt-2 sm:text-5xl flex items-end ml-2 sm:mt-6 text-center font-normal text-black">
-              00{" "}
-              <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1 mt-6 sm:mr-10 text-gray-700">
-                Second
-              </span>
-            </h1>
-          </div>
-        </div>
-
-        {/* Content Price */}
-        <div className="flex mt-9 md:mt-8 justify-between w-full space-x-2">
-          <div className="shadow-lg h-16  flex-1 sm:mr-2 rounded-3xl relative flex-row sm:h-20 ">
-            <p className="text-xs md:text-sm font-normal  text-gray-700 absolute top-1 left-4">
-              Current Price
-            </p>
-            <h2 className="text-sm mt-8 ml-4 md:text-lg font-bold text-center md:mt-10 ">
-              LDX = 0.1 BUSD
-            </h2>
-          </div>
-          <div className="shadow-lg flex-1  rounded-3xl relative flex-row md:h-20 ">
-            <p className="text-xs md:text-sm font-normal text-gray-700 absolute top-1 left-4">
-              Availble LDX
-            </p>
-            <h2 className="text-sm mt-8 md:text-lg font-bold text-center md:mt-10">
-              6,253,800.679 LDX
-            </h2>
-          </div>
-        </div>
-        <div className="flex mt-9 md:mt-8 justify-between space-x-2">
-          <div className="shadow-lg flex-1   rounded-3xl relative flex-row h-20 ">
-            <p className="text-xs md:text-sm font-normal  text-gray-700 absolute top-1 left-4">
-              LDX Token Sold
-            </p>
-            <h2 className="text-sm w-full ml-3 md:text-lg font-bold text-center mt-10 ">
-              00,000,000,000 LDX
-            </h2>
-          </div>
-          <div className="shadow-lg flex-1 rounded-3xl relative flex-row md:h-20 ">
-            <p className="text-xs md:text-sm font-normal text-gray-700 absolute top-1 left-4">
-              LDX Token Sold ($)
-            </p>
-            <h2 className="text-sm w-full md:text-lg font-bold text-center mt-10">
-              $0,000,000.000 USD
-            </h2>
-          </div>
-        </div>
-        {/* ICO Progress */}
-        <div className="w-full  mt-2 h-32 p-4  shadow-lg rounded-3xl">
-          <p className=" text-xs md:text-sm font-normal text-center ">
-            ICO Progress
-          </p>
-          {/* diagram */}
-          <div className="w-12/12 mt-2 rounded-3xl h-10 bg-gray-300">
-            <div
-              className="w-2/5 rounded-3xl h-10 "
-              style={{
-                background:
-                  "linear-gradient(to bottom, #1ec01e, #1dba1d, #1cb31c, #1bad1b, #1aa71a)",
-              }}
-            >
-              <p className="text-sm text-white text-center pt-2">30.98%</p>
-            </div>
-          </div>
-
-          <div className="relative mt-3 w-12/12">
-            <p className=" text-xs md:text-sm text-black  absolute bot-0">
-              Avaliable : 8885854.41 LDX
-            </p>
-            <p className="text-xs md:text-sm text-black  absolute bot-0 right-0">
-              Total : 10000000.00 LDX
-            </p>
+    <div>
+      <div className="flex items-center mt-4">
+        <div className="flex flex-row items-end shadow-lg rounded-xl px-4 py-2 bg-white dark:bg-gray-900">
+          <p className="text-lg text-gray-600 flex-row">Switcher</p>
+          <div className="flex-row px-2">
+            <button
+              onClick={() => handleChangeTheme("light")}
+              className="bg-gray-400 rounded-full focus:outline-none w-4 h-4 mr-2 flex-row"
+            ></button>
+            <button
+              onClick={() => handleChangeTheme("dark")}
+              className="bg-black rounded-full focus:outline-none w-4 h-4 flex-row"
+            ></button>
           </div>
         </div>
       </div>
 
-      <CoinCard />
+      <div className="max-w-sm mr-0 block sm:flex sm:justify-between sm:max-w-full sm:py-16 sm:px-8 py-4 px-4 sm:space-x-8">
+        <div className="mb-8  flex-1 flex-col sm:ml-3 sm:flex-row  ">
+          {/* content realtime */}
+          <div className="flex justify-between space-x-0.5 sm:space-x-6 max-w-xs sm:w-full ">
+            <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg dark:bg-black  ">
+              <h1
+                className="text-2xl mt-2 sm:text-5xl flex items-end ml-4 sm:mt-6 text-center font-normal text-black dark:text-gray-300
+dark:text-gray-300"
+              >
+                01{" "}
+                <span className="text-xs text-center mb-2 sm:text-sm flex-row flex-1 ml-1 mt-6 sm:mr-12 text-gray-700 dark:text-gray-300 ">
+                  Day
+                </span>
+              </h1>
+            </div>
+            <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg dark:bg-black  ">
+              <h1
+                className="text-2xl mt-2 sm:text-5xl flex items-end ml-2 sm:mt-6 text-center font-normal text-black dark:text-gray-300
+dark:text-gray-300"
+              >
+                01{" "}
+                <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1 ml-1 mt-6 sm:mr-12 text-gray-700 dark:text-gray-300">
+                  Hours
+                </span>
+              </h1>
+            </div>
+            <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg dark:bg-black  ">
+              <h1
+                className="text-2xl mt-2 sm:text-5xl flex items-end ml-2 sm:mt-6 text-center font-normal text-black dark:text-gray-300
+dark:text-gray-300"
+              >
+                00{" "}
+                <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1  mt-6 sm:mr-10 text-gray-700 dark:text-gray-300">
+                  Minute
+                </span>
+              </h1>
+            </div>
+            <div className="rounded-3xl flex-1 sm:h-20 shadow-lg dark:bg-black   ">
+              <h1
+                className="text-2xl mt-2 sm:text-5xl flex items-end ml-2 sm:mt-6 text-center font-normal text-black dark:text-gray-300
+dark:text-gray-300"
+              >
+                00{" "}
+                <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1 mt-6 sm:mr-10 text-gray-700 dark:text-gray-300">
+                  Second
+                </span>
+              </h1>
+            </div>
+          </div>
+
+          {/* Content Price */}
+          <div className="flex mt-9 md:mt-8 justify-between w-full space-x-2">
+            <div className="shadow-lg h-16  flex-1 sm:mr-2 rounded-3xl dark:bg-black   relative flex-row sm:h-20 ">
+              <p className="text-xs md:text-sm font-normal  text-gray-700 dark:text-gray-300 absolute top-1 left-4">
+                Current Price
+              </p>
+              <h2
+                className="text-sm mt-8 dark:text-gray-300
+dark:text-gray-300 ml-4 md:text-lg font-bold text-center md:mt-10 "
+              >
+                LDX = 0.1 BUSD
+              </h2>
+            </div>
+            <div className="shadow-lg flex-1  rounded-3xl dark:bg-black  relative flex-row md:h-20 ">
+              <p className="text-xs md:text-sm font-normal text-gray-700 dark:text-gray-300 absolute top-1 left-4">
+                Availble LDX
+              </p>
+              <h2
+                className="text-sm mt-8 dark:text-gray-300
+dark:text-gray-300 md:text-lg font-bold text-center md:mt-10"
+              >
+                6,253,800.679 LDX
+              </h2>
+            </div>
+          </div>
+          <div className="flex mt-9 md:mt-8 justify-between space-x-2">
+            <div className="shadow-lg flex-1   rounded-3xl dark:bg-black   relative flex-row h-20 ">
+              <p className="text-xs md:text-sm font-normal  text-gray-700 dark:text-gray-300 absolute top-1 left-4">
+                LDX Token Sold
+              </p>
+              <h2 className="text-sm w-full dark:text-gray-300 ml-3 md:text-lg font-bold text-center mt-10 ">
+                00,000,000,000 LDX
+              </h2>
+            </div>
+            <div className="shadow-lg flex-1 rounded-3xl dark:bg-black   relative flex-row md:h-20 ">
+              <p className="text-xs md:text-sm font-normal text-gray-700 dark:text-gray-300 absolute top-1 left-4">
+                LDX Token Sold ($)
+              </p>
+              <h2 className="text-sm w-full dark:text-gray-300 md:text-lg font-bold text-center mt-10">
+                $0,000,000.000 USD
+              </h2>
+            </div>
+          </div>
+          {/* ICO Progress */}
+          <div className="w-full  mt-2 h-32 p-4  shadow-lg rounded-3xl dark:bg-black  ">
+            <p className=" text-xs md:text-sm font-normal text-center dark:text-gray-300 ">
+              ICO Progress
+            </p>
+            {/* diagram */}
+            <div className="w-12/12 mt-2 rounded-3xl h-10 bg-gray-300">
+              <div
+                className="w-2/5 rounded-3xl h-10 "
+                style={{
+                  background:
+                    "linear-gradient(to bottom, #1ec01e, #1dba1d, #1cb31c, #1bad1b, #1aa71a)",
+                }}
+              >
+                <p className="text-sm text-white text-center pt-2">30.98%</p>
+              </div>
+            </div>
+
+            <div className="relative mt-3 w-12/12">
+              <p className=" text-xs md:text-sm text-black dark:text-gray-300  absolute bot-0">
+                Avaliable : 8885854.41 LDX
+              </p>
+              <p className="text-xs md:text-sm text-black dark:text-gray-300  absolute bot-0 right-0">
+                Total : 10000000.00 LDX
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <CoinCard />
+      </div>
     </div>
   );
 }
--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -6,12 +6,12 @@ import arrowDown2 from "../assets/arrowSvg.svg";
 
 const CoinCard = () => {
   return (
-    <div className="p-4 rounded-xl h-full flex-2 sm:flex-row flex-col text-center content-center sm:mt-0 bg-white dark:bg-main-dark shadow-xl">
+    <div className="p-4 rounded-xl h-full flex-2 sm:flex-row flex-col text-center content-center sm:mt-0  dark:bg-black shadow-xl">
       <div className="flex flex-col sm:flex-row sm:justify-between sm:space-x-4 sm:items-end space-y-4">
-        <div className="flex flex-row space-x-2">
-          <button className="bg-white dark:bg-main-comp-dark shadow-xl p-3 flex items-center rounded-xl justify-start space-x-2 w-34 focus:outline-none">
+        <div className="flex flex-row space-x-2 ">
+          <button className="bg-white dark:bg-main-comp-dark dark:bg-black shadow-xl p-3 flex items-center rounded-xl justify-start space-x-2 w-34 focus:outline-none">
             <img className="w-8 h-8" src={USDT} alt="icon-usdt" />
-            <span className="text-base font-semibold text-main-text-dark">
+            <span className="text-base font-semibold text-main-text-dark dark:text-white">
               USDT
             </span>
             <div className="w-5 h-5">
@@ -25,10 +25,10 @@ const CoinCard = () => {
         </div>
         <form>
           <div className="flex justify-between pb-2 px-2">
-            <span className="text-xs text-main-text-light dark:text-main-text-dark-2">
+            <span className="text-xs text-main-text-light dark:text-main-text-dark-2 dark:text-white">
               Pay
             </span>
-            <span className="text-xs text-main-text-light dark:text-main-text-dark-2">
+            <span className="text-xs text-main-text-light dark:text-main-text-dark-2 dark:text-white">
               Balance : 0.00
             </span>
           </div>
@@ -54,28 +54,28 @@ const CoinCard = () => {
         </div>
       </div>
       <div className="flex flex-col sm:flex-row sm:justify-between sm:items-end space-y-4">
-        <div className="flex flex-row space-x-2">
-          <div className="bg-white dark:bg-main-comp-dark shadow-xl p-3 flex items-center rounded-xl justify-start space-x-2 w-32">
+        <div className="flex flex-row space-x-2 ">
+          <div className="bg-white dark:bg-main-comp-dark dark:bg-black  shadow-xl p-3 flex items-center rounded-xl justify-start space-x-2 w-32">
             <div className="w-8 h-8">
               <img className="w-8 h-8" src={Litedex} alt="icon-usdt" />
             </div>
-            <span className="text-base font-semibold text-main-text-dark">
+            <span className="text-base font-semibold text-main-text-dark dark:text-white">
               LDX
             </span>
           </div>
         </div>
         <form>
           <div className="flex justify-between pb-2 px-2">
-            <span className="text-xs text-main-text-light dark:text-main-text-dark-2">
+            <span className="text-xs text-main-text-light dark:text-main-text-dark-2 dark:text-white">
               Get (Estimate)
             </span>
-            <span className="text-xs text-main-text-light dark:text-main-text-dark-2">
+            <span className="text-xs text-main-text-light dark:text-main-text-dark-2 dark:text-white">
               Balance : 0.00
             </span>
           </div>
           <div className="bg-main-light dark:bg-main-comp-dark border border-litedex p-4 flex sm:w-96 rounded-xl items-center space-x-2 flex-row">
             <input
-              className="text-base text-gray-400 flex-grow outline-none bg-transparent placeholder-main-text-light-2 dark:placeholder-main-text-dark"
+              className="text-base text-gray-400 flex-grow outline-none bg-transparent placeholder-main-text-light-2 dark:placeholder-main-text-dark dark:border-gray-500"
               type="number"
               placeholder="0.00"
             />
@@ -84,21 +84,21 @@ const CoinCard = () => {
       </div>
       <div className="flex flex-col sm:flex-row sm:justify-between sm:items-end space-y-4">
         <div className="flex flex-row space-x-2">
-          <div className="bg-white dark:bg-main-comp-dark p-3 hidden sm:flex items-center rounded-xl justify-start space-x-2 w-32">
-            <span className="text-sm font-semibold text-main-text-dark">
+          <div className="bg-white dark:bg-main-comp-dark p-3 dark:bg-black hidden sm:flex items-center rounded-xl justify-start space-x-2 w-32">
+            <span className="text-sm font-semibold text-main-text-dark dark:text-white">
               Referral Code
             </span>
           </div>
         </div>
         <form>
-          <div className="flex justify-start pb-2 px-2">
-            <span className="text-xs text-main-text-light dark:text-main-text-dark-2">
+          <div className="flex justify-start pb-2 px-2 ">
+            <span className="text-xs text-main-text-light dark:text-main-text-dark-2 dark:text-white">
               Referral Code (optional)
             </span>
           </div>
           <div className="bg-main-light dark:bg-main-comp-dark border border-litedex p-4 flex sm:w-96 rounded-xl items-center space-x-2 flex-row">
             <input
-              className="text-base text-gray-400 flex-grow outline-none bg-transparent placeholder-main-text-light-2 dark:placeholder-main-text-dark"
+              className="text-base text-gray-400 flex-grow outline-none bg-transparent placeholder-main-text-light-2 dark:placeholder-main-text-dark dark:border-gray-500"
               type="number"
               placeholder="0.00"
             />
