cc5893ec6e541a2821965b540cc325a31edc2b0c
rhobbikhaidir <rhobbie09@gmail.com>
2021-11-29 12:29:31 +0700

    fixing styling

--- src/App.js
+++ src/App.js
@@ -35,50 +35,38 @@ function App() {
         </div>
       </div>
 
-      <div className="max-w-sm mr-0 block sm:flex sm:justify-between sm:max-w-full sm:py-16 sm:px-8 py-4 px-4 sm:space-x-8">
+      <div className="max-w-sm mr-0 block sm:flex sm:justify-between sm:max-w-full sm:py-16 sm:px-8 py-4 px-4 sm:space-x-4">
         <div className="mb-8  flex-1 flex-col sm:ml-3 sm:flex-row  ">
           {/* content realtime */}
-          <div className="flex justify-between space-x-0.5 sm:space-x-6 max-w-xs sm:w-full ">
-            <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg dark:bg-black  ">
-              <h1
-                className="text-2xl text-center tex mt-2 sm:text-5xl flex items-end ml-4 sm:mt-6 text-center font-normal text-black dark:text-gray-300
-dark:text-gray-300"
-              >
+          <div className="flex justify-between space-x-0.5 sm:space-x-5 max-w-xs sm:w-full ">
+            <div className="rounded-3xl  sm:h-20 flex-1 flex-row items-center shadow-lg dark:bg-black pb-4 ">
+              <h1 className="text-2xl sm:mb-4 mt-2 sm:text-5xl sm:flex inline-block ml-4 sm:mt-6 text-center font-normal text-black dark:text-gray-300">
                 01{" "}
-                <span className="text-xs text-center mb-2 sm:text-sm flex-row flex-1 ml-1 mt-6 sm:mr-12 text-gray-700 dark:text-gray-300 ">
+                <span className="text-xs text-center mb-2 sm:text-sm sm:flex-row flex-1 ml-1 mt-6 sm:mr-12 text-gray-700 dark:text-gray-300 ">
                   Day
                 </span>
               </h1>
             </div>
-            <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg dark:bg-black  ">
-              <h1
-                className="text-2xl mt-2 text-center sm:text-5xl flex items-end ml-2 sm:mt-6 text-center font-normal text-black dark:text-gray-300
-dark:text-gray-300"
-              >
+            <div className="rounded-3xl  sm:h-20 flex-1 flex-row items-center shadow-lg dark:bg-black  px-1 ">
+              <h1 className="text-2xl mt-2  sm:text-5xl inline-block sm:flex flex-1  ml-4 sm:mt-6 text-center font-normal text-black dark:text-gray-300">
                 01{" "}
-                <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1 ml-1 mt-6 sm:mr-12 text-gray-700 dark:text-gray-300">
+                <span className="text-xs text-center mb-2 sm:text-sm sm:flex-row  flex-1 ml-1 mt-6 sm:mr-12 text-gray-700 dark:text-gray-300">
                   Hours
                 </span>
               </h1>
             </div>
-            <div className="rounded-3xl  sm:h-20 flex-1 flex-row  shadow-lg dark:bg-black  ">
-              <h1
-                className="text-2xl mt-2 sm:text-5xl text-center flex items-end ml-2 sm:mt-6 text-center font-normal text-black dark:text-gray-300
-dark:text-gray-300"
-              >
+            <div className="rounded-3xl  sm:h-20 flex-1 flex-row  items-center shadow-lg dark:bg-black  ">
+              <h1 className="text-2xl mt-2 sm:text-5xl inline-block sm:flex  ml-4 sm:mt-6 text-center font-normal text-black dark:text-gray-300">
                 00{" "}
-                <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1  mt-6 sm:mr-10 text-gray-700 dark:text-gray-300">
+                <span className="text-xs text-center mb-2 sm:text-sm sm:flex-row   mt-6 sm:mr-10 text-gray-700 dark:text-gray-300">
                   Minute
                 </span>
               </h1>
             </div>
-            <div className="rounded-3xl flex-1 sm:h-20 shadow-lg dark:bg-black   ">
-              <h1
-                className="text-2xl mt-2 sm:text-5xl text-center flex items-end ml-2 sm:mt-6 text-center font-normal text-black dark:text-gray-300
-dark:text-gray-300"
-              >
+            <div className="rounded-3xl flex-1 sm:h-20 shadow-lg items-center dark:bg-black   ">
+              <h1 className="text-2xl mt-2 sm:text-5xl inline-block sm:flex ml-4 sm:mt-6 text-center font-normal text-black dark:text-gray-300">
                 00{" "}
-                <span className="text-xs text-center mb-2 sm:text-sm flex-row  flex-1 mt-6 sm:mr-10 text-gray-700 dark:text-gray-300">
+                <span className="text-xs text-center mb-2 sm:text-sm sm:flex-row  items-center flex-1 mt-6 sm:mr-10 text-gray-700 dark:text-gray-300">
                   Second
                 </span>
               </h1>
@@ -86,15 +74,12 @@ dark:text-gray-300"
           </div>
 
           {/* Content Price */}
-          <div className="flex mt-9 md:mt-8 justify-between w-full space-x-2">
+          <div className="flex mt-4 sm:mt-4 justify-between w-full space-x-2">
             <div className="shadow-lg h-16  flex-1 sm:mr-2 rounded-3xl dark:bg-black   relative flex-row sm:h-20 ">
               <p className="text-xs md:text-sm font-normal  text-gray-700 dark:text-gray-300 absolute top-1 left-4">
                 Current Price
               </p>
-              <h2
-                className="text-sm mt-8 dark:text-gray-300
-dark:text-gray-300 ml-4 md:text-lg font-bold text-center md:mt-10 "
-              >
+              <h2 className="text-sm mt-8 dark:text-gray-300 ml-4 md:text-lg font-bold text-center md:mt-10 ">
                 LDX = 0.1 BUSD
               </h2>
             </div>
@@ -102,15 +87,12 @@ dark:text-gray-300 ml-4 md:text-lg font-bold text-center md:mt-10 "
               <p className="text-xs md:text-sm font-normal text-gray-700 dark:text-gray-300 absolute top-1 left-4">
                 Availble LDX
               </p>
-              <h2
-                className="text-sm mt-8 dark:text-gray-300
-dark:text-gray-300 md:text-lg font-bold text-center md:mt-10"
-              >
+              <h2 className="text-sm mt-8 dark:text-gray-300 md:text-lg font-bold text-center md:mt-10">
                 6,253,800.679 LDX
               </h2>
             </div>
           </div>
-          <div className="flex mt-9 md:mt-8 justify-between space-x-2">
+          <div className="flex mt-4 sm:mt-4 justify-between space-x-2">
             <div className="shadow-lg flex-1   rounded-3xl dark:bg-black   relative flex-row h-20 ">
               <p className="text-xs md:text-sm font-normal  text-gray-700 dark:text-gray-300 absolute top-1 left-4">
                 LDX Token Sold
@@ -129,7 +111,7 @@ dark:text-gray-300 md:text-lg font-bold text-center md:mt-10"
             </div>
           </div>
           {/* ICO Progress */}
-          <div className="w-full  mt-2 h-32 p-4  shadow-lg rounded-3xl dark:bg-black  ">
+          <div className="w-full  mt-2 h-32 p-4  shadow-lg rounded-3xl dark:bg-black sm:mt-5  ">
             <p className=" text-xs md:text-sm font-normal text-center dark:text-gray-300 ">
               ICO Progress
             </p>
